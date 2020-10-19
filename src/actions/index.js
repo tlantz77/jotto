73a903359a2fb512b6e131f1644f217e85c371a2
Tim Lantz <tlantz@gmail.com>
2020-10-19 17:11:34 -0500

    add secretWord action creator and reducer with tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,19 @@
+import axios from 'axios';
+
 import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
 	CORRECT_GUESS: 'CORRECT_GUESS',
-	GUESS_WORD: 'GUESS_WORD',
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD'
 };
 
 export const guessWord = (guessedWord) => {
-	return function (dispatch, getState) {
+	return (dispatch, getState) => {
 		const secretWord = getState().secretWord;
 		const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
-		dispatch({
+		dispatch({  
 			type: actionTypes.GUESS_WORD,
 			payload: { guessedWord, letterMatchCount },
 		});
@@ -20,3 +23,15 @@ export const guessWord = (guessedWord) => {
 		}
 	};
 };
+
+export const getSecretWord = () => { 
+  return (dispatch) => {
+    return axios.get('http://localhost:3030')
+      .then(response => {
+        dispatch({
+          type: actionTypes.SET_SECRET_WORD,
+          payload: response.data
+        });
+      });
+  }
+}
