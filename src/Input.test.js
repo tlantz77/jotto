add4b72d9352d84a1cbfb13e38e186cf4d1200ca
Tim Lantz <tlantz@gmail.com>
2020-10-20 17:10:40 -0500

    finish jotto

--- src/Input.test.js
+++ src/Input.test.js
@@ -95,4 +95,8 @@ describe('guessWord action creator call', () => {
     const guessWordArg = guessWordMock.mock.calls[0][0];
     expect(guessWordArg).toBe(guessedWord);
   });
+
+  it('should clear input box on submit', () => {
+    expect(wrapper.state('currentGuess')).toBe('');
+  });
 });
