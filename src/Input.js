1467357fe3958097d9d3f15bd84d5902214b7e69
Tim Lantz <tlantz@gmail.com>
2020-10-20 16:31:37 -0500

    add test for Input guess word action creator call

--- src/Input.js
+++ src/Input.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import { guessWord } from './actions';
 
-class Input extends Component {
+export class UnconnectedInput extends Component {
 	render() {
 		const contents = this.props.success ? null : (
 			<form className='form-inline'>
@@ -15,7 +15,8 @@ class Input extends Component {
 				/>
 				<button
 					data-test='submit-button'
-					className='btn btn-primary mb-2'
+          className='btn btn-primary mb-2'
+          onClick={() => this.props.guessWord('train')}
 					type='submit'>
 					Submit
 				</button>
@@ -29,4 +30,4 @@ const mapStateToProps = ({ success }) => {
 	return { success };
 };
 
-export default connect(mapStateToProps, { guessWord })(Input);
+export default connect(mapStateToProps, { guessWord })(UnconnectedInput);
--- src/Input.test.js
+++ src/Input.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
-import Input from './Input';
+import Input, { UnconnectedInput } from './Input';
 
 const setup = (initialState = {}) => {
 	const store = storeFactory(initialState);
@@ -74,4 +74,19 @@ describe('redux props', () => {
     const guessWordProp = wrapper.instance().props.guessWord;
     expect(guessWordProp).toBeInstanceOf(Function);
   });
+});
+
+describe('guessWord action creator call', () => {
+  it('should run guessWord action creator when form is submitted', () => {
+    const guessWordMock = jest.fn();
+    const props = {
+      success: false,
+      guessWord: guessWordMock
+    };
+    const wrapper = shallow(<UnconnectedInput {...props} />);
+    findByTestAttr(wrapper, 'submit-button').simulate('click');
+    const guessWordMockCallCount = guessWordMock.mock.calls.length;
+    expect(guessWordMockCallCount).toBe(1);
+  });
+    
 });
