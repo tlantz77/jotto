161a57e54e000ca2b9aca1199fad958753681120
Tim Lantz <tlantz@gmail.com>
2020-10-20 17:11:51 -0500

    remove secret word display

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ export class UnconnectedApp extends Component {
     return (
       <div className='container'>
         <h1>Jotto</h1>
-        <div>The secret word is {this.props.secretWord}</div>
         <Congrats success={this.props.success} />
         <Input/>
         <GuessedWords guessedWords={this.props.guessedWords} />
